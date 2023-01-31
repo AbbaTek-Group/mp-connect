8293eb76859bfd0acd9d1b2f2b08efe83d59a0e7
evnwttn <evnwttn@gmail.com>
2023-01-31 14:30:20 -0330

    Update IconBox.tsx

--- mp-connect/src/components/landing/content/IconBox.tsx
+++ mp-connect/src/components/landing/content/IconBox.tsx
@@ -1,15 +1,31 @@
 import { Box } from "@mui/material";
 import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import MoreTimeIcon from "@mui/icons-material/MoreTime";
+import GroupWorkIcon from "@mui/icons-material/GroupWork";
 import { inline } from "../../../styles";
 import { IconBoxProps } from "../../../types";
 
 export const IconBox = ({ id }: IconBoxProps) => {
+  // renders icon & text conditionally according to id
+
   return (
     <Box sx={inline.secondDivSx.iconBox.container}>
       <Box>
-        <TaskAltIcon sx={inline.secondDivSx.iconBox.icon} />
+        {id === "check" ? (
+          <TaskAltIcon sx={inline.secondDivSx.iconBox.icon} />
+        ) : id === "hourglass" ? (
+          <MoreTimeIcon sx={inline.secondDivSx.iconBox.icon} />
+        ) : (
+          <GroupWorkIcon sx={inline.secondDivSx.iconBox.icon} />
+        )}
       </Box>
-      <Box sx={inline.secondDivSx.iconBox.textBox}>{id}</Box>
+      <Box sx={inline.secondDivSx.iconBox.textBox}>
+        {id === "check"
+          ? "checkdasdasd"
+          : id === "hourglass"
+          ? "hourglasszzzz"
+          : "groupygroupy"}
+      </Box>
     </Box>
   );
 };
