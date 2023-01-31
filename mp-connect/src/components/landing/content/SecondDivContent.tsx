37eebc50cc4a84c18a3729b602bf0eafef57dbcf
evnwttn <evnwttn@gmail.com>
2023-01-31 15:05:11 -0330

    Global div text styling

--- mp-connect/src/components/landing/content/SecondDivContent.tsx
+++ mp-connect/src/components/landing/content/SecondDivContent.tsx
@@ -7,7 +7,18 @@ export const SecondDivContent = () => {
     <Box sx={inline.secondDivSx.container}>
       <Box sx={inline.secondDivSx.globeDiv}>
         <Box sx={inline.secondDivSx.globeDiv.imageBox}></Box>
-        <Box sx={inline.secondDivSx.globeDiv.textBox}></Box>
+        <Box sx={inline.secondDivSx.globeDiv.textBox}>
+          <Box sx={inline.secondDivSx.globeDiv.textBox.title}>
+            A global <b>solution</b> to a global problem.
+          </Box>
+          <Box sx={inline.secondDivSx.globeDiv.textBox.body}>
+            Our mission is to allow for <b>scalable</b> and <b>comparable </b>
+            research and monitoring of microplastics. We do this by applying
+            machine learning to automate physical identification,
+            characterization and measurement of microplastics particles,
+            enabling repeatable <b>results without the expertise</b>.
+          </Box>
+        </Box>
       </Box>
       <Box sx={inline.secondDivSx.iconBoxDiv}>
         <Box sx={inline.secondDivSx.iconBoxDiv.rowFlexbox}>
