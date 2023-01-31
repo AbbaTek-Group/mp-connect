341057fd4bb848e24d7477376bedc1127ad30087
evnwttn <evnwttn@gmail.com>
2023-01-31 14:09:39 -0330

    Organizing SecondDiv Sx

--- mp-connect/src/components/landing/content/SecondDivContent.tsx
+++ mp-connect/src/components/landing/content/SecondDivContent.tsx
@@ -1,76 +1,27 @@
 import { Box } from "@mui/material";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import { inline } from "../../../styles";
 
 const IconBox = () => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box>Icon</Box>
-      <Box sx={{ width: "95%", height: "65.6%", backgroundColor: "red" }}></Box>
+    <Box sx={inline.secondDivSx.iconBox.container}>
+      <Box>
+        <TaskAltIcon sx={inline.secondDivSx.iconBox.icon} />
+      </Box>
+      <Box sx={inline.secondDivSx.iconBox.textBox}></Box>
     </Box>
   );
 };
 
 export const SecondDivContent = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        ml: "7.3vw",
-        mr: "7.3vw",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          backgroundColor: "pink",
-          height: "33vh",
-          width: "81vw",
-          my: "24.4vh",
-        }}
-      >
-        <Box
-          sx={{ backgroundColor: "purple", height: "90%", width: "23%" }}
-        ></Box>
-        <Box
-          sx={{
-            backgroundColor: "red",
-            height: "90%",
-            width: "75.15%",
-          }}
-        ></Box>
+    <Box sx={inline.secondDivSx.container}>
+      <Box sx={inline.secondDivSx.globeDiv}>
+        <Box sx={inline.secondDivSx.globeDiv.imageBox}></Box>
+        <Box sx={inline.secondDivSx.globeDiv.textBox}></Box>
       </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          backgroundColor: "pink",
-          height: "67.7vh",
-          width: "100%",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            width: "100%",
-            height: "auto",
-          }}
-        >
+      <Box sx={inline.secondDivSx.iconBoxDiv}>
+        <Box sx={inline.secondDivSx.iconBoxDiv.rowFlexbox}>
           <IconBox />
           <IconBox />
           <IconBox />
