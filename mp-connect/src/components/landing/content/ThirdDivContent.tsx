aa2041f15bacd9b5638e0b6b931e8304e2dfe1d0
evnwttn <evnwttn@gmail.com>
2023-02-01 11:20:59 -0330

    Import Icons

--- mp-connect/src/components/landing/content/ThirdDivContent.tsx
+++ mp-connect/src/components/landing/content/ThirdDivContent.tsx
@@ -1,4 +1,6 @@
 import { Box, Button } from "@mui/material";
+import BubbleChartIcon from "@mui/icons-material/BubbleChart";
+import InsightsIcon from "@mui/icons-material/Insights";
 import { TextCard } from "./TextCard";
 // import micro from "./img/micro.png";
 import { inline } from "../../../styles";
@@ -6,6 +8,8 @@ import { inline } from "../../../styles";
 export const ThirdDivContent = () => {
   return (
     <Box sx={inline.thirdDivSx.container}>
+      <BubbleChartIcon />
+      <InsightsIcon />
       <Box>
         <TextCard id={"top"} />
         <TextCard id={"bottom"} />
