3a9be22495ca0ca760af9c95af7dbf8ab08cf652
evnwttn <evnwttn@gmail.com>
2023-02-02 15:18:51 -0330

    Push

--- mp-connect/src/components/login/content/img/LoginContent.tsx
+++ mp-connect/src/components/login/content/img/LoginContent.tsx
@@ -1,29 +1,20 @@
 import { Box, Button } from "@mui/material";
-// import { login } from "../../../../styles";
+import { login } from "../../../../styles";
 import graphic from "../img/graphic.png";
 
 export const LoginContent = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        width: "85.4vw",
-        mt: "6.5vw",
-      }}
-    >
-      <Box sx={{ width: "100%", height: "33.95vw" }}>
+    <Box sx={login.loginSx.container}>
+      <Box sx={login.loginSx.imgDiv.div}>
         <Box
           sx={{
+            ...login.loginSx.imgDiv,
             backgroundImage: `url(${graphic})`,
-            height: "100%",
-            backgroundSize: "100%",
           }}
         />
       </Box>
-      <Box sx={{ width: "100%", height: "33.95vw" }}>
-        <Box sx={{ backgroundColor: "pink", height: "100%", ml: "6.14vw" }}>
+      <Box sx={login.loginSx.formDiv.div}>
+        <Box sx={login.loginSx.formDiv}>
           <Box>Sign Into MPConnect</Box>
           <Box>
             <Box>Email Address</Box>
--- mp-connect/src/styles/login.ts
+++ mp-connect/src/styles/login.ts
@@ -9,7 +9,35 @@ export const loginSx = {
   flexDirection: "row",
   justifyContent: "center",
   alignItems: "center",
-  container: {},
-  imgDiv: {},
-  formDiv: {},
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "85.4vw",
+    mt: "6.5vw",
+  },
+  imgDiv: {
+    height: "100%",
+    backgroundSize: "100%",
+    div: {
+      width: "100%",
+      height: "33.95vw",
+    },
+  },
+  formDiv: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: palette.white,
+    borderRadius: "5px",
+    boxShadow: "0px 0px 16px rgb(0 0 0 / 0.16)",
+    height: "100%",
+    ml: "6.14vw",
+    title: {},
+    text: {},
+    signInButton: {},
+    forgotButton: {},
+    div: {
+      width: "100%",
+    },
+  },
 };
