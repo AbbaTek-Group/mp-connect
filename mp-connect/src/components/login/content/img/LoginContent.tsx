9b58ffed978fd1ef12f19673867060ea94b88d56
evnwttn <evnwttn@gmail.com>
2023-02-02 14:21:59 -0330

    Login Layout / Graphic

--- mp-connect/src/components/login/content/img/LoginContent.tsx
+++ mp-connect/src/components/login/content/img/LoginContent.tsx
@@ -1,12 +1,29 @@
 import { Box, Button } from "@mui/material";
-import { login } from "../../../../styles";
+// import { login } from "../../../../styles";
+import graphic from "../img/graphic.png";
 
 export const LoginContent = () => {
   return (
-    <Box sx={login.loginSx.container}>
-      <Box sx={login.loginSx.imgDiv}>Image</Box>
-      <Box sx={login.loginSx.formDiv}>
-        <Box>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        width: "85.4vw",
+        mt: "6.5vw",
+      }}
+    >
+      <Box sx={{ width: "100%", height: "33.95vw" }}>
+        <Box
+          sx={{
+            backgroundImage: `url(${graphic})`,
+            height: "100%",
+            backgroundSize: "100%",
+          }}
+        />
+      </Box>
+      <Box sx={{ width: "100%", height: "33.95vw" }}>
+        <Box sx={{ backgroundColor: "pink", height: "100%", ml: "6.14vw" }}>
           <Box>Sign Into MPConnect</Box>
           <Box>
             <Box>Email Address</Box>
--- mp-connect/src/styles/login.ts
+++ mp-connect/src/styles/login.ts
@@ -1,18 +1,15 @@
 // login inline styling
 
 import { palette } from "./palette";
-// import { font } from "./font";
+import { font } from "./font";
 
 export const loginSx = {
-  backgroundColor: palette.trueWhite,
-  container: {
-    ml: "7.3vw",
-    mr: "7.3vw",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
+  width: "100vw",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
+  container: {},
   imgDiv: {},
   formDiv: {},
 };
