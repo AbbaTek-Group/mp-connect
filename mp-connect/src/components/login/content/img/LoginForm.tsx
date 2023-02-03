00f16161703efd0c3039c6c29a2514decc39a89f
evnwttn <evnwttn@gmail.com>
2023-02-03 12:27:34 -0330

    LoginForm func

--- mp-connect/src/components/login/content/img/LoginForm.tsx
+++ mp-connect/src/components/login/content/img/LoginForm.tsx
@@ -1,7 +1,19 @@
+import { useState, useEffect, useRef } from "react";
 import { Box, Button, TextField } from "@mui/material";
 import { login } from "../../../../styles";
+import { LoginProps } from "../../../../types";
 
 export const LoginForm = () => {
+  const [loginFormData, setLoginFormData] = useState<LoginProps>();
+  const emailField = useRef<HTMLInputElement>();
+  const passwordField = useRef<HTMLInputElement>();
+
+  useEffect(() => {
+    if (loginFormData) {
+      console.log(loginFormData);
+    }
+  }, [loginFormData]);
+
   return (
     <Box>
       <Box sx={login.loginSx.formDiv.title}>
@@ -9,10 +21,27 @@ export const LoginForm = () => {
       </Box>
       <Box sx={login.loginSx.formDiv.form}>
         <Box>Email</Box>
-        <TextField id="email" variant="filled" />
+        <TextField id="email" inputRef={emailField} variant="filled" />
         <Box>Password</Box>
-        <TextField id="password" type="password" variant="filled" />
-        <Button sx={login.loginSx.formDiv.signInButton}>Sign In</Button>
+        <TextField
+          id="password"
+          inputRef={passwordField}
+          type="password"
+          variant="filled"
+        />
+        <Button
+          sx={login.loginSx.formDiv.signInButton}
+          onClick={() => {
+            if (emailField.current!.value && passwordField.current!.value) {
+              setLoginFormData({
+                email: emailField.current!.value,
+                password: passwordField.current!.value,
+              });
+            }
+          }}
+        >
+          Sign In
+        </Button>
         <Button sx={login.loginSx.formDiv.forgotButton}>Forgot Password</Button>
       </Box>
     </Box>
--- mp-connect/src/types/index.ts
+++ mp-connect/src/types/index.ts
@@ -1,3 +1,4 @@
-export type { NavScrollFxProps } from "./NavScrollFx";
 export type { IconBoxProps } from "./IconBox";
+export type { LoginProps } from "./LoginProps";
+export type { NavScrollFxProps } from "./NavScrollFx";
 export type { TextCardProps } from "./TextCard";
