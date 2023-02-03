3b603ab47af4db864e247048381e10fdf596d653
evnwttn <evnwttn@gmail.com>
2023-02-03 12:02:00 -0330

    Update NavBarContent.tsx

--- mp-connect/src/components/landing/content/NavBarContent.tsx
+++ mp-connect/src/components/landing/content/NavBarContent.tsx
@@ -1,5 +1,6 @@
+import { Link } from "react-router-dom";
 import { Box, Button } from "@mui/material";
-import { landing } from "../../../styles";
+import { landing, palette } from "../../../styles";
 import logo from "./img/mpconnectlogo.svg";
 
 export const NavBarContent = () => {
@@ -13,18 +14,15 @@ export const NavBarContent = () => {
         />
       </Box>
       <Box>
-        <Button
-          sx={landing.navBarContentSx.loginButton}
-          variant="contained"
-          disableRipple
-        >
-          Login
+        <Button sx={landing.navBarContentSx.loginButton} variant="contained">
+          <Link
+            style={{ textDecoration: "none", color: palette.black }}
+            to="/login"
+          >
+            Login
+          </Link>
         </Button>
-        <Button
-          sx={landing.navBarContentSx.registerButton}
-          variant="contained"
-          disableRipple
-        >
+        <Button sx={landing.navBarContentSx.registerButton} variant="contained">
           <a href="https://abbatekgroup.us12.list-manage.com/subscribe?u=e7a7fdd7e7a0cd0d46a303521&id=7383ff5a34">
             Register
           </a>
