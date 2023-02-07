4c66420749cfc8944d62abb41fbc0a04ddfd4fd2
evnwttn <evnwttn@gmail.com>
2023-02-07 10:02:32 -0330

    Update loginHandler.ts

--- mp-connect/src/handlers/loginHandler.ts
+++ mp-connect/src/handlers/loginHandler.ts
@@ -1,5 +1,19 @@
+import axios from "axios";
 import { LoginProps } from "../types";
 
 export async function loginHandler(user: LoginProps) {
-  console.log(user);
+  const route = `http://localhost:5000/user`;
+
+  axios
+    .put(
+      `${route}`,
+      { loginData: user },
+      {
+        withCredentials: true,
+      }
+    )
+    .then((data: any) => console.log(data))
+    .catch(function (error: any) {
+      console.log(error);
+    });
 }
