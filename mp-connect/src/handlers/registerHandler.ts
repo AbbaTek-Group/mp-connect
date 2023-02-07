21b5b73506511a30228370c314032833ac7f9c70
evnwttn <evnwttn@gmail.com>
2023-02-07 14:12:54 -0330

    Update registerHandler.ts

--- mp-connect/src/handlers/registerHandler.ts
+++ mp-connect/src/handlers/registerHandler.ts
@@ -1,21 +1,19 @@
-// import axios from "axios";
+import axios from "axios";
 import { RegisterProps } from "../types";
 
 export async function registerHandler(user: RegisterProps) {
-  //   const route = `http://localhost:5000/user`;
+  const route = `http://localhost:5000/user`;
 
-  console.log(user);
-
-  //   axios
-  //     .post(
-  //       `${route}`,
-  //       { userLoginData: user },
-  //       {
-  //         withCredentials: true,
-  //       }
-  //     )
-  //     .then((data: any) => console.log(data))
-  //     .catch(function (error: any) {
-  //       console.log(error);
-  //     });
+  axios
+    .post(
+      `${route}`,
+      { userRegisterData: user },
+      {
+        withCredentials: true,
+      }
+    )
+    .then((data: any) => console.log(data))
+    .catch(function (error: any) {
+      console.log(error);
+    });
 }
