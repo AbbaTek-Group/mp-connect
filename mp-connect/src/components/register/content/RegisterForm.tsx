660c7afc6536a81c675f26740cd13f66fb61ffd8
evnwttn <evnwttn@gmail.com>
2023-02-07 11:02:23 -0330

    Update RegisterForm.tsx

--- mp-connect/src/components/register/content/RegisterForm.tsx
+++ mp-connect/src/components/register/content/RegisterForm.tsx
@@ -1,5 +1,24 @@
-import { Box } from "@mui/material";
+import { useRef } from "react";
+import { Box, Button, TextField } from "@mui/material";
 
 export const RegisterForm = () => {
-  return <Box>RegisterForm</Box>;
+  const emailField = useRef<HTMLInputElement>();
+  const passwordField = useRef<HTMLInputElement>();
+
+  return (
+    <Box>
+      <Box>
+        <Box>Email</Box>
+        <TextField id="email" inputRef={emailField} variant="filled" />
+        <Box>Password</Box>
+        <TextField
+          id="password"
+          inputRef={passwordField}
+          type="password"
+          variant="filled"
+        />
+        <Button>Register</Button>
+      </Box>
+    </Box>
+  );
 };
